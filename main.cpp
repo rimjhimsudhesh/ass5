c20cc6a6433b3e07acd14bc1dbbe7f20273d2910
rimjhimsudhesh <110277428+rimjhimsudhesh@users.noreply.github.com>
2023-06-01 23:10:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
-#include "threadedTree.h" // Assuming you have a header file for the threadedTree class
+#include "threadedTree.h" 
+#include <cassert>
+
+using namespace std;
+
+void testThreadedTree() {
+    threadedTree tree(5);
+
+    // Test adding nodes
+    tree.add(9);
+    tree.remove(2);
+    tree.remove(1);
+
+    //Iterator iterator;
+    //iterator.inorderTraverse(tree);
+    
+
+    // Test contains() method
+    std::cout << "Tree contains 4: " << (tree.contains(4) ? "true" : "false") << std::endl;
+    std::cout << "Tree contains 2: " << (tree.contains(2) ? "true" : "false") << std::endl;
+    std::cout << "Tree contains 9: " << (tree.contains(9) ? "true" : "false") << std::endl;
+    std::cout << "Tree contains 1: " << (tree.contains(1) ? "true" : "false") << std::endl;
+    //cout << "here";
+
+}
 
 int main() {
-    // Create a threaded tree with some nodes
-    threadedTree myTree(5);
-    /* myTree.insert(5);
-    myTree.insert(3);
-    myTree.insert(7);
-    myTree.insert(2);
-    myTree.insert(4);
-    myTree.insert(6);
-    myTree.insert(8);
- */
-    // Print the tree
-    std::cout << "Original Tree:" << std::endl;
-    std::cout << myTree << std::endl;
-
-    // Clear the tree
-    //myTree.clear(5);
-
-    // Print the tree after clearing
-    std::cout << "Tree after clearing:" << std::endl;
-    std::cout << myTree << std::endl;
+    testThreadedTree();
 
     return 0;
 }
+
+
+
+
+
+
 
 
 
